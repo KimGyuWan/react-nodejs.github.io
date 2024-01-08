53d1c8af630a8df9ed900cea790cd2c88f5772a5
SANHOKIM <awrko@naver.com>
2024-01-08 19:29:36 +0900

    컴포넌트 리팩토링 (코드배열객체 메소드 map 적용함)

--- src/components/sanho/AppSectionCsr.js
+++ src/components/sanho/AppSectionCsr.js
@@ -1,124 +1,41 @@
 import React from "react";
 import datajson from "../../data/data.json";
+import "../../scss/sanho.scss";
 
 function AppSectionCsr() {
+  const shuffledTaglist = datajson.sectioncsr.content;
+
   return (
     <section id="csr">
-      <div class="container">
-        <h2 class="">CSR 캠페인</h2>
-
-        <div class="box1">
-          <ul class="article-list d-flex justify-content-between">
-            <li class="show">
-              <a href="">
-                <div class="img-box">
-                  <img src="./img/sanho/csr-1.jpg" alt="펫푸드" />
-                </div>
-              </a>
-              <h3>
-                <a href="">
-                  "하림펫푸드, 반려동물과 함께하는 '펫푸드 공장 투어 프로그램'
-                  재개 [멍멍냥냥]"
-                </a>
-              </h3>
-              <div class="csr-tag d-flex">
-                <a href="">#해피댄스스튜디오</a>
-                <a href="">#투어프로그램</a>
-                <a href="">#개슐랭</a>
-              </div>
-            </li>
-
-            <li class="show">
-              <a href="">
-                <div class="img-box">
-                  <img src="./img/sanho/csr-2.jpg" alt="펫푸드" />
-                </div>
-              </a>
-              <h3>
-                <a href="">
-                  "펫푸드 사업부터 사회공헌까지 …지금은 펫휴머니제이션 시대"
-                </a>
-              </h3>
-              <div class="csr-tag d-flex">
-                <a href="">#하림펫푸드</a>
-                <a href="">#펫휴머지네이션</a>
-                <a href="">#사회공헌</a>
-              </div>
-            </li>
-
-            <li class="show">
-              <a href="">
-                <div class="img-box">
-                  <img src="./img/sanho/csr-3.jpg" alt="펫푸드" />
-                </div>
-              </a>
-              <h3>
-                <a href="">
-                  "하림펫푸드, 배우 이기우와 유기동물 사료 1톤 기부"
-                </a>
-              </h3>
-              <div class="csr-tag d-flex">
-                <a href="">#배우이기우</a>
-                <a href="">#유기동물</a>
-                <a href="">#사회공헌</a>
-                <a href="">#더리얼사료</a>
-              </div>
-            </li>
-
-            <li>
-              <a href="">
-                <div class="img-box">
-                  <img src="./img/sanho/csr-4.jpg" alt="펫푸드" />
-                </div>
-              </a>
-              <h3>
-                <a href="">
-                  "양양 멍비치, '더 리얼 아이스크림' 부스로 여름 즐기기!"
-                </a>
-              </h3>
-              <div class="csr-tag d-flex">
-                <a href="">#멍비치</a>
-                <a href="">#더리얼</a>
-                <a href="">#여름이벤트</a>
-              </div>
-            </li>
-
-            <li>
-              <a href="">
-                <div class="img-box">
-                  <img src="./img/sanho/csr-5.jpg" alt="펫푸드" />
-                </div>
-              </a>
-              <h3>
-                <a href="">
-                  "하림펫푸드, 출범 5년만에 매출 366억원…휴먼그레이드 통했다"
-                </a>
-              </h3>
-              <div class="csr-tag d-flex">
-                <a href="">#휴먼그레이드</a>
-                <a href="">#더리얼</a>
-                <a href="">#AAFCO</a>
-              </div>
-            </li>
-
-            <li>
-              <a href="">
-                <div class="img-box">
-                  <img src="./img/sanho/csr-6.jpg" alt="펫푸드" />
-                </div>
-              </a>
-              <h3>
-                <a href="">
-                  "버거킹 독퍼, 하림펫푸드와 맞손…프리미엄 강아지 간식으로
-                  재탄생"
-                </a>
-              </h3>
-              <div class="csr-tag d-flex">
-                <a href="">#리얼독퍼</a>
-                <a href="">#100%휴먼그레이드</a>
-                <a href="">#버거킹콜라보레이션</a>
-              </div>
-            </li>
+      <div className="container">
+        <h2 className="">{datajson.sectioncsr.h2}</h2>
+        <div className="box1">
+          <ul className="article-list d-flex justify-content-between">
+            {shuffledTaglist.map(
+              (el, idx) =>
+                idx < 3 && (
+                  <li className="show" key={idx}>
+                    <a href={el.href}>
+                      <div className="img-box">
+                        <img src={el.src} alt="펫푸드"></img>
+                      </div>
+                    </a>
+                    <h3>
+                      <a href={el.href}>{el.h3}</a>
+                    </h3>
+                    <div className="csr-tag d-flex">
+                      {el.csrtag.map((eel, iidx) => {
+                        return (
+                          <a href={el.href} key={iidx}>
+                            {eel.nm}
+                          </a>
+                        );
+                      })}
+                    </div>
+                  </li>
+                  // AppSectionCsr.js 에
+                )
+            )}
           </ul>
         </div>
       </div>
