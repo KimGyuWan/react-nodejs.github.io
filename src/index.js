1337094bafcf583adbc411c13c93d06ad1073727
SANHOKIM <awrko@naver.com>
2024-01-08 19:04:09 +0900

    jsx를 사용하여 정적객체 html요소를 파싱하고 js함수호출변환시켜 리액트엔진이  가상 dom에 렌더링 처리하여 표현하도록 함

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Routes } from "react-router-dom";
 
 import AppSectionCsr from "./components/sanho/AppSectionCsr";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Routes>
+  <>
     <AppSectionCsr />
-  </Routes>
+  </>
 );
