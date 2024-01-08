5b0bd03c180c0b285d450ace2871288d63bdef35
SANHOKIM <awrko@naver.com>
2024-01-08 17:13:17 +0900

    index.js 에 AppSectionCsr 컴포넌트로 연결

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Routes } from "react-router-dom";
+
+import AppSectionCsr from "./components/sanho/AppSectionCsr";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <App />
-  </>
+  <Routes>
+    <AppSectionCsr />
+  </Routes>
 );
