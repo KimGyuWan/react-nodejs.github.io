7344984e0f544c326533a1115f0366ecd829c5d2
snb2323 <dheltus1r@naver.com>
2024-01-11 16:42:20 +0900

    map 사용하여 Brand 기업설명 swiper slide 구현및 scss수정

--- src/component/dongmin/Brand.js
+++ src/component/dongmin/Brand.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectFade, Autoplay } from 'swiper/modules';
-
+import { Navigation, EffectFade, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/swiper-bundle.css';
 import './dm.scss';
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -14,51 +15,65 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function Brand(props) {
     return (
         <>
-            <h2 style={{ display: 'flex', lineHeight: '1.5em', }}>{props.data.sectionbrand.h2}</h2>
-            <div className='Br postion-relative d-flex  justify-content-space-between;'>
+            <section id="section-brand">
+                <div className='our position-rel'>
+                    <h2 >{props.data.sectionbrand.h2}</h2>
+                    <h2 >{props.data.sectionbrand.h2_1}</h2>
+
+                </div>
 
 
                 <Swiper
-                    modules={[EffectFade, Autoplay]} effect="fade"
+                    modules={[Navigation, EffectFade, Autoplay]} effect="fade"
                     loop={true}
+                    rewind={true}
+
+                    navigation={true}
 
 
-                    // autoplay={{
-                    //     delay: 2500,
-                    // }}
+
+                    autoplay={{
+                        delay: 2500,
+                    }}
 
                     spaceBetween={50}
                     slidesPerView={1}
 
 
-                    navigation
                     className='brand-list'
                 >
 
                     {props.data.sectionbrand.brandlist.map((brand, index) => (
-                        <div key={index}>
-                            <SwiperSlide className='bg-white '>
-                                <div className='brand-item postion-relative'>
-                                    <img src={brand.src} alt={`하림펫푸드 로고 ${index + 1}`}>
-                                    </img>
+                        <div class="swiper" id="mainSwiper">
+                            <div class="swiper-wrapper" key={index}>
+                                <div class="swiper-slide postion-rel d-flex align-items-center justify-content-center ">
+                                    <SwiperSlide className='bg-white '>
+                                        <div className='position-relative'>
+                                            <img src={brand.src} alt={`하림펫푸드 로고 ${index + 1}`}>
+                                            </img>
+                                        </div>
+
+                                        <div className='sh position-relative'>
+                                            {brand.h3 && brand.h3}
+
+                                            <div className='dp position-rel'>
+                                                {brand.p && brand.p}
+                                            </div>
+
+                                        </div>
+
+
+
+
+                                    </SwiperSlide>
+
                                 </div>
-
-                                <div>
-                                    <h3>{brand.h3}</h3>
-                                    <br />
-
-                                    <p>{brand.p}</p>
-                                    <br />
-
-                                </div>
-
-                            </SwiperSlide>
-
+                            </div>
                         </div>
-
                     ))}
+
                 </Swiper >
-            </div >
+            </section>
         </>
     );
 }
