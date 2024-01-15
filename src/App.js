829f42cd028a0ea87d28d88a509d0679fa970183
KimGyuWan <y5g5776@naver.com>
2024-01-15 17:07:58 +0900

    동민이 merge 후 파일 정리

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Item from './component/gyuwan/Item'
 import Instagram from './component/gyuwan/Instagram';
 import AppSectionCsr from "./component/sanho/AppSectionCsr";
 import Lysfooter from './component/yeonsoolee/Lysfooter';
-
+import datainfo from './data/data.json'
+import Brand from './component/dongmin/Brand';
+import Form from './component/dongmin/Form';
 function App() {
   return (
     <>
       <Lysswiper />
       <Lyslinebanner />
+      <Brand data={datainfo}></Brand>
       <Item />
       <Instagram />
       <AppSectionCsr />
+      <Form />
       <Lysfooter />
     </>
   );
