11dfcbae5457006668beaefd5d12726ead45f1a3
joons5967 <dlwnstjd5967@nate.com>
2024-01-16 10:47:39 +0900

    기존 자바스크립트 이용해서 만든 헤더틀을 리액트로 바꿔서 틀을 변경

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import datainfo from "./data/data.json"
+import Header from './component/joonsung/Header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App />
-  </>
+
+  <Header dataAd={datainfo.top_banner[0]} dataGnb={datainfo.gnb}></Header>
+</>
 );
-
-
