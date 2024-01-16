0eafde62249d1234998f66740597b611fbcabfe3
KimGyuWan <y5g5776@naver.com>
2024-01-16 11:30:03 +0900

    준성이 브랜치 머지 후 파일정리

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Header from './component/joonsung/Header';
 import Lysswiper from './component/yeonsoolee/Lysswiper';
 import Lyslinebanner from './component/yeonsoolee/Lyslinebanner';
 import Brand from './component/dongmin/Brand';
@@ -6,9 +7,11 @@ import Instagram from './component/gyuwan/Instagram';
 import AppSectionCsr from "./component/sanho/AppSectionCsr";
 import Lysfooter from './component/yeonsoolee/Lysfooter';
 import Form from './component/dongmin/Form';
+import datainfo from "./data/data.json"
 function App() {
   return (
     <>
+      <Header dataAd={datainfo.top_banner[0]} dataGnb={datainfo.gnb}></Header>
       <Lysswiper />
       <Lyslinebanner />
       <Brand></Brand>
