ca033708589edb90aa58dfa6005f9adf3c404b33
SANHOKIM <awrko@naver.com>
2024-01-25 18:03:56 +0900

    feat:Appsection.js에 레이아웃 부트스트랩 추가/ enhance:sanho.scss 요소노드 별로 데이터 값 개선

--- src/component/sanho/AppSectionCsr.js
+++ src/component/sanho/AppSectionCsr.js
@@ -14,27 +14,35 @@ function AppSectionCsr() {
       <div className="container">
         <h2 className="">{datajson.sectioncsr.h2}</h2>
         <div className="box1">
-          <ul className="article-list d-lg-flex justify-content-between">
+          <ul className="article-list d-lg-flex justify-content-between align-items-stretch">
             {shuffledTaglist.map(
               (el, idx) =>
                 idx < 3 && (
-                  <li className="show col-md-4" key={idx}>
+                  <li className="show col-md-4 d-flex flex-column " key={idx}>
                     <a href={el.href}>
                       <div className="img-box">
                         <img src={el.src} alt="펫푸드"></img>
                       </div>
                     </a>
-                    <h3>
-                      <a href={el.href}>{el.h3}</a>
-                    </h3>
-                    <div className="csr-tag d-flex">
-                      {el.csrtag.map((eel, iidx) => {
-                        return (
-                          <a href={el.href} key={iidx}>
-                            {eel.nm}
-                          </a>
-                        );
-                      })}
+
+                    <div className="flex-grow-1 d-flex flex-column justify-content-between">
+                      <h3>
+                        <a href={el.href}>{el.h3}</a>
+                      </h3>
+
+                      <div className="csr-tag">
+                        <ul className="">
+                          {el.csrtag.map((eel, iidx) => {
+                            return (
+                              <li className="d-block mb-0">
+                                <a href={el.href} key={iidx}>
+                                  {eel.nm}
+                                </a>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
                     </div>
                   </li>
                 )
